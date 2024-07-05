c90bad137890abbd7223d8e3d8d41f21ef3bad09
Ignitz <moritz.pertl@gmx.at>
2024-07-05 11:24:27 +0200

    image for app

--- AndroidApp/kotlin/myapplication/src/main/java/com/example/myapplication/widgets/HomeWidgets.kt
+++ AndroidApp/kotlin/myapplication/src/main/java/com/example/myapplication/widgets/HomeWidgets.kt
@@ -5,6 +5,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -16,21 +19,27 @@ import com.example.myapplication.viewmodels.LoginViewModel
 import com.example.myapplication.viewmodels.ScoreboardViewModel
 
 @Composable
-fun HomeScreenButtons (navController: NavController, loginViewModel: LoginViewModel, gameViewModel: GameViewModel, scoreboardViewModel: ScoreboardViewModel, homeViewModel: HomeViewModel) {
+fun HomeScreenButtons(
+    navController: NavController,
+    loginViewModel: LoginViewModel,
+    gameViewModel: GameViewModel,
+    scoreboardViewModel: ScoreboardViewModel,
+    homeViewModel: HomeViewModel
+) {
     val padding = 10.dp
 
-    Column (
-        modifier = Modifier.fillMaxWidth(),
+    Column(
+        modifier = Modifier.fillMaxWidth().verticalScroll(rememberScrollState()),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Spacer(Modifier.padding(20.dp))
         QuickPlayButton(navController, gameViewModel)
         Spacer(Modifier.padding(padding))
-        FriendPlayButton(homeViewModel,navController)
+        FriendPlayButton(homeViewModel, navController)
         Spacer(Modifier.padding(padding))
         RankedPlayButton(navController, gameViewModel)
         Spacer(Modifier.padding(padding))
-        ScoreboardButton(navController, scoreboardViewModel = scoreboardViewModel )
+        ScoreboardButton(navController, scoreboardViewModel = scoreboardViewModel)
         Spacer(Modifier.padding(padding))
         SettingsButton(navController)
         Spacer(Modifier.padding(padding))
